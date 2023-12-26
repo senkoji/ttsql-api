c09eca0c8d2a8711d93742eedcd99cdf46e49a92
skj <kazuki.senkoji@gmail.com>
2023-12-26 13:24:52 +0900

    Connect to DB server.

--- src/fast_api/main.py
+++ src/fast_api/main.py
@@ -1,12 +1,33 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import openai
 import os
+from sqlalchemy import create_engine, Column, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from starlette.middleware.cors import CORSMiddleware
+import random
+import string
 
 # OpenAI APIキーの設定
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
+
+# SQLAlchemy設定
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:~1000koji+1ki@127.0.0.1:3306/ttsqlpjt?charset=utf8mb4"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# モデルの定義
+class SQLInfo(Base):
+    __tablename__ = "sqlinfo"
+    user_id = Column(String(3), primary_key=True, index=True)
+    sqlformat = Column(Text, index=True)
+
+# データベース初期化
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -25,11 +46,27 @@ class SQLRequest(BaseModel):
 @app.post("/hoge")
 async def generate_sql(request: SQLRequest):
     try:
+        # OpenAIからの応答を取得
         response = openai.Completion.create(
-            model="text-davinci-003",  # モデルの選択（最新のモデル名に更新してください）
+            model="text-davinci-003",
             prompt=f"In a database with a schema named '{request.sqlformat}', to extract data with the content '{request.sqlcontent}', what SQL query should be written? Please output the SQL statement.",
-            max_tokens=1000  # 応答の最大トークン数
+            max_tokens=1000
         )
-        return {"sql_query": response.choices[0].text.strip()}
+        sql_query = response.choices[0].text.strip()
+
+        # データベースにデータを挿入
+        db = SessionLocal()
+        try:
+            user_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
+            db_sqlinfo = SQLInfo(user_id=user_id, sqlformat=request.sqlformat)
+            db.add(db_sqlinfo)
+            db.commit()
+        finally:
+            db.close()
+
+        return {"sql_query": sql_query}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        # より具体的なエラーハンドリング
+        error_message = f"予期せぬエラーが発生しました: {type(e).__name__} - {e}"
+        print(error_message)
+        return JSONResponse(status_code=500, content={"detail": error_message})
